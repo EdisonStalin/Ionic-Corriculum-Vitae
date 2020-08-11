3b5a19afb2783fe0efcf739b9707e2d9722036a9
EdisonStalin <edison.jumbo@epn.edu.ec>
2020-08-11 15:05:44 -0500

    2020_ionic

---
+++ Deber1/src/assets/perfil/Ionic App Archivos/tab1-tab1-module.js
@@ -0,0 +1,141 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tab1-tab1-module"],{
+
+/***/ "./node_modules/raw-loader/dist/cjs.js!./src/app/tab1/tab1.page.html":
+/*!***************************************************************************!*\
+  !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/tab1/tab1.page.html ***!
+  \***************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("\n<ion-card class=\"title\">\n    <ion-card-header>\n      <ion-card-subtitle>Ferfil</ion-card-subtitle>\n      <ion-card-title>Edisom S. Jumbo</ion-card-title>\n    </ion-card-header>\n    <img src=\"../../assets/perfil/perfil.png\" height=\"150px\" width=\"150px\"/>\n    <ion-card-content>\n      \n     <p>Estudiante de la carrera de Analisis de Sistemas Informáticos de la \n       Escuela Politecnica Nacional. </p>\n    </ion-card-content>\n</ion-card>\n\n<ion-card class=\"content\">\n  <ion-item class=\"ion-activated\">\n    <ion-icon name=\"call\" slot=\"start\"></ion-icon>\n    <ion-label>+593 963107510</ion-label>\n  </ion-item>\n\n  <ion-item >\n    <ion-icon name=\"location\" slot=\"start\"></ion-icon>\n    <ion-label>Quto,Ecuador</ion-label>\n  </ion-item>\n\n  <ion-item class=\"ion-activated\">\n    <ion-icon name=\"mail\" slot=\"start\"></ion-icon>\n    <ion-label>edisonsjumbo@gmail.com</ion-label>\n  </ion-item>\n\n  <ion-item>\n    <ion-icon name=\"logo-github\" slot=\"start\"></ion-icon>\n    <ion-label>EdisonStalin</ion-label>\n  </ion-item>\n</ion-card>\n\n");
+
+/***/ }),
+
+/***/ "./src/app/tab1/tab1-routing.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/tab1/tab1-routing.module.ts ***!
+  \*********************************************/
+/*! exports provided: Tab1PageRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab1PageRoutingModule", function() { return Tab1PageRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+/* harmony import */ var _tab1_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./tab1.page */ "./src/app/tab1/tab1.page.ts");
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _tab1_page__WEBPACK_IMPORTED_MODULE_3__["Tab1Page"],
+    }
+];
+let Tab1PageRoutingModule = class Tab1PageRoutingModule {
+};
+Tab1PageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+    })
+], Tab1PageRoutingModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/tab1/tab1.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/tab1/tab1.module.ts ***!
+  \*************************************/
+/*! exports provided: Tab1PageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab1PageModule", function() { return Tab1PageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/__ivy_ngcc__/fesm2015/forms.js");
+/* harmony import */ var _tab1_page__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./tab1.page */ "./src/app/tab1/tab1.page.ts");
+/* harmony import */ var _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../explore-container/explore-container.module */ "./src/app/explore-container/explore-container.module.ts");
+/* harmony import */ var _tab1_routing_module__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./tab1-routing.module */ "./src/app/tab1/tab1-routing.module.ts");
+
+
+
+
+
+
+
+
+let Tab1PageModule = class Tab1PageModule {
+};
+Tab1PageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+        imports: [
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonicModule"],
+            _angular_common__WEBPACK_IMPORTED_MODULE_3__["CommonModule"],
+            _angular_forms__WEBPACK_IMPORTED_MODULE_4__["FormsModule"],
+            _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_6__["ExploreContainerComponentModule"],
+            _tab1_routing_module__WEBPACK_IMPORTED_MODULE_7__["Tab1PageRoutingModule"]
+        ],
+        declarations: [_tab1_page__WEBPACK_IMPORTED_MODULE_5__["Tab1Page"]]
+    })
+], Tab1PageModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/tab1/tab1.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/tab1/tab1.page.scss ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("* {\n  display: block;\n  text-align: center;\n}\n\nimg {\n  border-radius: 50%;\n  display: block;\n  margin-left: auto;\n  margin-right: auto;\n}\n\np {\n  text-align: center;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvdGFiMS9DOlxcVXNlcnNcXEVkaXNvbiBKdW1ib1xcRGVza3RvcFxcdGVzdGluZ1xcRGViZXIxL3NyY1xcYXBwXFx0YWIxXFx0YWIxLnBhZ2Uuc2NzcyIsInNyYy9hcHAvdGFiMS90YWIxLnBhZ2Uuc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNJLGNBQUE7RUFDQSxrQkFBQTtBQ0NKOztBREdBO0VBQ0ksa0JBQUE7RUFDQSxjQUFBO0VBQ0osaUJBQUE7RUFDQSxrQkFBQTtBQ0FBOztBREdBO0VBQ0ksa0JBQUE7QUNBSiIsImZpbGUiOiJzcmMvYXBwL3RhYjEvdGFiMS5wYWdlLnNjc3MiLCJzb3VyY2VzQ29udGVudCI6WyIqe1xyXG4gICAgZGlzcGxheTogYmxvY2s7XHJcbiAgICB0ZXh0LWFsaWduOiBjZW50ZXI7XHJcblxyXG59XHJcblxyXG5pbWcge1xyXG4gICAgYm9yZGVyLXJhZGl1czogNTAlO1xyXG4gICAgZGlzcGxheTogYmxvY2s7XHJcbm1hcmdpbi1sZWZ0OiBhdXRvO1xyXG5tYXJnaW4tcmlnaHQ6IGF1dG87XHJcbiAgfVxyXG5cclxucCB7XHJcbiAgICB0ZXh0LWFsaWduOiBjZW50ZXI7XHJcblxyXG59IiwiKiB7XG4gIGRpc3BsYXk6IGJsb2NrO1xuICB0ZXh0LWFsaWduOiBjZW50ZXI7XG59XG5cbmltZyB7XG4gIGJvcmRlci1yYWRpdXM6IDUwJTtcbiAgZGlzcGxheTogYmxvY2s7XG4gIG1hcmdpbi1sZWZ0OiBhdXRvO1xuICBtYXJnaW4tcmlnaHQ6IGF1dG87XG59XG5cbnAge1xuICB0ZXh0LWFsaWduOiBjZW50ZXI7XG59Il19 */");
+
+/***/ }),
+
+/***/ "./src/app/tab1/tab1.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/tab1/tab1.page.ts ***!
+  \***********************************/
+/*! exports provided: Tab1Page */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab1Page", function() { return Tab1Page; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+
+
+let Tab1Page = class Tab1Page {
+    constructor() { }
+};
+Tab1Page = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+        selector: 'app-tab1',
+        template: Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(/*! raw-loader!./tab1.page.html */ "./node_modules/raw-loader/dist/cjs.js!./src/app/tab1/tab1.page.html")).default,
+        styles: [Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(/*! ./tab1.page.scss */ "./src/app/tab1/tab1.page.scss")).default]
+    })
+], Tab1Page);
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=tab1-tab1-module.js.map
